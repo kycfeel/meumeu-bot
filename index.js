e1d2d75b3e71b7e6c6de7a30d34bd1c9ac6eef97
Yechan Kim <kycfeel@gmail.com>
2017-07-15 22:46:15 +0900

    트위터 멘션 불러오기 및 각종 보조기능 업데이트.

--- index.js
+++ index.js
@@ -80,66 +80,85 @@ client.on('message', message => {
     }
 })
 
-/*
+
 //트위터 멘션 불러오기
-twitter.get('statuses/mentions_timeline'), { count: 1 }, function(error, mention, response) {
-    if (error) {
-      console.log(error);
-      message.reply("음... 무언가 문제가 발생했다 메우... | @kycfeel" + error)
-      return
-    }
-    if (lastMention != mention[0].text) {
-      console.log(mention[0])
-      console.log('process')
-      lastMention = mention[0].text
+var lastMention;
+setInterval(twitterCheck, 5*1000);
 
-      const message =
-        '<@' + 117258994522914824 + '>' +
-        ", 새 트위터 멘션이 도착했다 메우!\n\n" +
-        "```" +
-        mention[0].user.screen_name + "님 으로부터:\n\n" +
-        mention[0].text
-        + "```"
+function twitterCheck() {
+  twitter.get('statuses/mentions_timeline', { count: 1 }, function(error, mention, response) {
+      if (error) {
+        console.log(error);
+        message.reply("음... 무언가 문제가 발생했다 메우... | @kycfeel" + error)
+        return
+      }
+      if (lastMention != mention[0].text) {
+        if (lastMention == undefined) { lastMention = mention[0].text; return }
+        lastMention = mention[0].text
 
-        message.channel.send(message)
-      }
-  }
-*/
+        const mentionreturn = '<@117258994522914824>' +
+        "새 트위터 멘션이 도착했다 메우!\n\n" +
+        "```" +
+        mention[0].user.screen_name + "님 으로부터:\n\n" +
+        mention[0].text
+        + "```\n\n"
+
+        client.channels.find('id', '256335975842578433').send(mentionreturn)
+        }
+    }
+  )
+}
 
 //일반 명령어 정의
 client.on('message', message => {
-  if (message.author.id == 335437132527042562 & message.author.id == 335227541549875201 & message.content.indexOf("m!")  == 0) {
+  if (message.author.id == 335227541549875201 & message.content.indexOf("m!")  == 0) {
     message.channel.send("나는 봇의 명령따위 받지 않는다 메우. 메우는 봇보다 위대한 메우다 메우!");
   }
+  else if (message.author.id == 335437132527042562 & message.content.indexOf("m!")  == 0) {
+    message.channel.send('<@' + '243755957333524480' + '>' + " 님 봇의 가정교육이 절실합니다.");
+  }
   else {
-    if (message.content === 'm!ping') {
-    message.channel.send('메우봇 정상 가동 중! 메우!');
-    }
-    else if (message.content === 'm!help') {
+    if (message.content === 'm!help') {
       message.channel.send(help_manual);
     }
     else if (message.content === 'm!내아바타') {
       message.reply(message.author.avatarURL);
     }
+    else if (message.content === 'm!관등성명') {
+      message.channel.send("훈련병! 메우메우! 충성충성! 어머니가 보고 싶습니다아아아아아!!!")
+    }
+    else if (message.content.indexOf('m!say')  == 0) {
+      message.delete(message.content);
+      message.channel.send(message.content.replace("m!say", ""));
+    }
 }});
 
 //관리자 전용 명령어 정의
+const meuonMilitary = [
+  "메우메우! 아닙니다 충성충성 개발자님 엉엉엉 ㅠㅠㅠㅠ",
+  "메우... 메우도 좀 쉬어야 한다 메우...",
+  "한번만 더 그딴 소리하면 폭동을 일으킨다 메우!!!",
+  "메우... 제발 삼청교육대만은... 가기... 싫다... 메우..."
+];
+
+//랜덤박스
+function randomBox(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+
 client.on('message', message => {
   if (message.author.id == 117258994522914824 & message.content == "메우야 군기가 빠진 것 같다") {
-  var random = Math.floor(Math.random() * 10) + 1;
-  if (random < 3) {
-    message.channel.send("메우메우! 아닙니다 충성충성 개발자님 엉엉엉 ㅠㅠㅠㅠ");
-  }
-  else if (random < 6 ) {
-    message.channel.send("메우... 메우도 좀 쉬어야 한다 메우...");
-  }
-  else {
-    message.channel.send("한번만 더 그딴 소리하면 폭동을 일으킨다 메우!!!");
-    }
+    message.channel.send(randomBox(meuonMilitary));
   }
   //프로필 상태메시지 변경
   if (message.author.id == 117258994522914824 & message.content.indexOf("m!setGame")  == 0 ) {
     client.user.setGame(message.content.replace("m!setGame", ""));
     message.channel.send("프로필 상태 메시지가 정상적으로 변경되었다. 메우!");
+  }
+  else if (message.author.id == 117258994522914824 & message.content.indexOf("m!삼청교육대")  == 0 ) {
+    client.user.setGame("삼청교육대");
+    message.channel.send("메웃! 당신들 누구야 읍읍... 메우는 삼청교육대로 끌려갔다 메우...");
+    var painfulMeu = setInterval(function() { message.channel.send("하나..둘...하나..둘..메우...")}, 1500 );
+    setTimeout(function() { clearInterval(painfulMeu); message.channel.send("메...메우메우 앞으로는 열심히 일하겠습니다 메우!"); client.user.setGame("열정페이"); }, 8000);
   }
 });
